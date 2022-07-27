589b0b9f72efba6ff14b431680166bf4090eb92e
Rui Neto <ruineto11@gmail.com>
2022-07-27 05:40:42 -0300

    chore: get response on api movies endpoint

--- src/App.java
+++ src/App.java
@@ -1,12 +1,21 @@
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
-
     // 1. Make the HTTP connection and search the top 250 movies
     String url = "https://api.mocki.io/v2/549a5d8b";
-    HttpClient client = HttpClient.newHttpClient();
+    URI uri = URI.create(url);
+    var client = HttpClient.newHttpClient();
+    var request = HttpRequest.newBuilder(uri).GET().build();
+
+    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+    String body = response.body();
+
+    System.out.println(body);
 
     // 2. Extract only the data that matters (title, poster, rating)
 
